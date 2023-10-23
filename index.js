1b0ba86cb48821d47d406485fcff55ee34fd111b
George Suarez <georgejsuarez@gmail.com>
2023-10-23 14:49:32 -0700

    UI elements are now dynamically updating every 5 seconds

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
+const names = ["Bob", "Alice", "Carol", "George", "Jose", "Tom"];
+const prices = [51, 20, 93, 121, 39, 12, 45, 112, 75];
+const occupations = ["Programmer", "Truck Driver", "Gardner", "Teacher", "Writer"];
 const people = [
     {
         name: "Dr.Slice",
         price: 25,
-        occupation: "gardner",
+        occupation: "Gardner",
     },
     {
         name: "Dr.Pressure",
         price: 51,
-        occupation: "programmer",
+        occupation: "Programmer",
     },
-    {
-        name: "Alice",
-        price: 72,
-        occupation: "writer",
-    },
-    {
-        name: "Bob",
-        price: 95,
-        occupation: "teacher",
-    },
-    {
-        name: "Carol",
-        price: 112,
-        occupation: "programmer",
-    }
 ];
 
-const addFreelancerIntervalId = setInterval(addFreelancer, 3000);
+const addFreelancerIntervalId = setInterval(addFreelancer, 5000);
 
 render();
 
@@ -35,11 +23,31 @@ function render() {
     const freelancers = document.querySelector("#freelancers");
     const freelancerElements = people.map((person) => {
         const element = document.createElement("li");
+        element.textContent = `Name: ${person.name} Occupation: ${person.occupation} Price: ${person.price}`;
+        element.classList.add("freelancer");
+        // freelancers.appendChild(element);
         return element;
     });
     freelancers.replaceChildren(...freelancerElements);
+
+    // Average price
+    const avgPrice = document.querySelector("#avgPrice");
+    const averagePrice = getAveragePrice();
+    avgPrice.textContent = `Average Starting Price: ${averagePrice}`;
+}
+
+function getAveragePrice() {
+    return Math.round(people.reduce((accum, currentPrice) => accum + currentPrice.price, 0) / people.length);
 }
 
 function addFreelancer() {
+    const name = names[Math.floor(Math.random() * names.length)];
+    const price = prices[Math.floor(Math.random() * prices.length)];
+    const occupation = occupations[Math.floor(Math.random() * occupations.length)];
+    people.push({name: name, price: price, occupation: occupation});
     render();
+    
+    if (people.length >= 10) {
+        clearInterval(addFreelancerIntervalId);
+    }
 }
